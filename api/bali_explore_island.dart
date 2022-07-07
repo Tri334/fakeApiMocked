cc5c8b9f9af177177a5c3481a818cb8c878b6686
Dewa Gede Trika Meranggi <50865141+Tri334@users.noreply.github.com>
2022-07-07 13:52:46 +0800

    add fake data for bali_explore_island

---
+++ api/bali_explore_island.dart
@@ -0,0 +1,29 @@
+var bali_explore_island = {
+  "Nearby": {
+    "place": {
+      "string": ["Tirta Gangga", "Ujung Kulon"]
+    },
+    "distant": {"regexp": "[1-9][0-9]{1,3}[m]{1}"},
+    "img": {
+      "string": [
+        "assets/images/tirtagangga.jpg",
+        "assets/images/ujungkulon.JPG"
+      ]
+    }
+  },
+  "Popular": {
+    "place": {
+      "string": ["Kelingking Beach", "Garuda Wisnu", "Lembongan Beach"]
+    },
+    "map": {
+      "string": ["Nusa Penida", "Denpasar", "Nusa Lembongan"]
+    },
+    "img": {
+      "string": [
+        "assets/images/landingPage.png",
+        "assets/images/gwk.jpg",
+        "assets/images/nusalembongan.jpg"
+      ]
+    }
+  }
+};
