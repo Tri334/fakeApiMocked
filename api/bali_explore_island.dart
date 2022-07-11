8d1b11875e3199cd2576d0c0bf0e8cffb1c8ffc8
Dewa Gede Trika Meranggi <50865141+Tri334@users.noreply.github.com>
2022-07-11 20:47:27 +0800

    Update bali_explore_island.dart

--- api/bali_explore_island.dart
+++ api/bali_explore_island.dart
@@ -1,45 +1,52 @@
 var bali_explore_island = {
-  "Nearby": {
-    "place": {
-      "string": ["Tirta Gangga", "Ujung Kulon"]
+  {
+    "Nearby": {
+      "place": {
+        "string": ["Tirta Gangga", "Ujung Kulon"]
+      },
+      "distant": {"regexp": "[1-9][0-9]{1,3}[m]{1}"},
+      "img": {
+        "string": [
+          "assets/images/tirtagangga.jpg,assets/images/tirtagangga1.jpg,assets/images/tirtagangga2.jpg",
+          "assets/images/ujungkulon.JPG,assets/images/ujungkulon1.JPG,assets/images/ujungkulon2.JPG"
+        ]
+      },
+      "map": {
+        "string": ["Klungkung", "Denpasar", "Nusa Dua"]
+      },
+      "desc": {
+        "string": [
+          "This place is awsome!!!",
+          "This is a strange place to be...",
+          "What a beautiful place!"
+        ]
+      },
+      "star": {"regexp": "[1-4][.][0-9]"},
+      "favorite": {"boolean": {}}
     },
-    "distant": {"regexp": "[1-9][0-9]{1,3}[m]{1}"},
-    "img": {
-      "string": [
-        "assets/images/tirtagangga.jpg",
-        "assets/images/ujungkulon.JPG"
-      ]
-    },
-    "desc": {
-      "string": [
-        "This place is awsome!!!",
-        "This is a strange place to be...",
-        "What a beautiful place!"
-      ]
-    },
-    "star": {"regexp": "[1-5][.][1-5]{3}"}
-  },
-  "Popular": {
-    "place": {
-      "string": ["Kelingking Beach", "Garuda Wisnu", "Lembongan Beach"]
-    },
-    "map": {
-      "string": ["Nusa Penida", "Denpasar", "Nusa Lembongan"]
-    },
-    "img": {
-      "string": [
-        "assets/images/landingPage.png",
-        "assets/images/gwk.jpg",
-        "assets/images/nusalembongan.jpg"
-      ]
-    },
-    "desc": {
-      "string": [
-        "This place is awsome!!!",
-        "This is a strange place to be...",
-        "What a beautiful place!"
-      ]
-    },
-    "star": {"regexp": "[1-5][.][1-5]{3}"}
+    "Popular": {
+      "place": {
+        "string": ["Kelingking Beach", "Garuda Wisnu", "Lembongan Beach"]
+      },
+      "map": {
+        "string": ["Nusa Penida", "Denpasar", "Nusa Lembongan"]
+      },
+      "img": {
+        "string": [
+          "assets/images/landingPage.png,assets/images/landing2.jpg,assets/images/landing3.jpg",
+          "assets/images/gwk.jpg,assets/images/gwk1.jpg,assets/images/gwk2.jpg",
+          "assets/images/nusalembongan.jpg,assets/images/nusalembongan1.jpg,assets/images/nusalembongan2.jpg"
+        ]
+      },
+      "desc": {
+        "string": [
+          "This place is awsome!!!",
+          "This is a strange place to be...",
+          "What a beautiful place!"
+        ]
+      },
+      "star": {"regexp": "[1-4][.][0-9]"},
+      "favorite": {"boolean": {}}
+    }
   }
 };
